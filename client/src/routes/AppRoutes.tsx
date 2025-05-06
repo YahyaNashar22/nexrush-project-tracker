1b9071a274936d213eb46a287a513e9818cc872d
YahyaNashar22 <yahyanashar22@gmail.com>
2025-05-06 15:07:25 +0300

    added initial setup

---
+++ client/src/routes/AppRoutes.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<p>hello</p>} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
